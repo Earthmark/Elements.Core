7d8562cdc54ad2c7c6fb8a8e21b2ea5ff80b4cc7
Earthmark <earthmark.miller@gmail.com>
2026-07-31 21:19:08 -0700

    Added deprecation markers to Elements.Core/Collections/ListEnumerableWrapper.cs

--- Elements.Core/Collections/ListEnumerableWrapper.cs
+++ Elements.Core/Collections/ListEnumerableWrapper.cs
@@ -9,12 +9,14 @@ namespace Elements.Core
 {
     // Wraps a list without allocations to give direct access to the list's struct enumerator, to prevent allocations
 
+    [Obsolete("Likely no longer needed with current .net versions")]
     public readonly struct ListEnumerableWrapper<T> : IEnumerable<T>
     {
         static List<T> emptyList;
 
         readonly List<T> list;
 
+        [Obsolete("Likely no longer needed with current .net versions")]
         public ListEnumerableWrapper(List<T> list)
         {
             if(list == null)
@@ -33,13 +35,16 @@ namespace Elements.Core
         IEnumerator<T> IEnumerable<T>.GetEnumerator() => GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        [Obsolete("Likely no longer needed with current .net versions")]
         public static implicit operator ListEnumerableWrapper<T>(List<T> list) => new ListEnumerableWrapper<T>(list);
     }
 
+    [Obsolete("Likely no longer needed with current .net versions")]
     public readonly struct SlimListEnumerableWrapper<T> : IEnumerable<T>
     {
         readonly SlimList<T> list;
 
+        [Obsolete("Likely no longer needed with current .net versions")]
         public SlimListEnumerableWrapper(SlimList<T> list)
         {
             this.list = list;
@@ -50,6 +55,7 @@ namespace Elements.Core
         IEnumerator<T> IEnumerable<T>.GetEnumerator() => GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        [Obsolete("Likely no longer needed with current .net versions")]
         public static implicit operator SlimListEnumerableWrapper<T>(SlimList<T> list) => new SlimListEnumerableWrapper<T>(list);
     }
 }
